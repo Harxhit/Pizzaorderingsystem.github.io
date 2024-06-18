bbf68940692ef3dd7b90761112d13bd931cd5399
Harshit Singh <86526868+Harxhit@users.noreply.github.com>
2024-06-18 20:45:54 +0530

    Update Pizza.py

--- Pizza.py
+++ Pizza.py
@@ -1,1 +1,22 @@
-Piiza
+print("--------------Thank you for choosing Python Pizza Deliveries!--------------")
+size = input(("Pizza size :")) 
+add_pepperoni = input(("add_pepperoni :")) 
+extra_cheese = input(("extra_cheese :")) 
+bill = 0 
+if size == "S" : 
+  bill += 15
+elif size == "M" : 
+  bill += 20
+else :
+  size == "L"
+bill += 25
+if add_pepperoni == "Y" :
+  size == "S"
+  bill += 2 
+elif add_pepperoni == "Y" :
+  size == "M" and "L"
+  bill += 3 
+if extra_cheese == "Y":
+  bill += 1
+print("Thank you for choosing Python Pizza Deliveries!")
+print(f"Your final bill is : ${bill}")
